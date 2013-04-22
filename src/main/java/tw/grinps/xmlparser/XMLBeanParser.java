23b4a07637b80ca376ba821788eb380300589c3f
kolyjjj <me.koly.li@gmail.com>
2013-04-22 19:46:35 +0800

    [Kewei] Refactor the xml bean parser

--- src/main/java/tw/grinps/xmlparser/XMLBeanParser.java
+++ src/main/java/tw/grinps/xmlparser/XMLBeanParser.java
@@ -17,29 +17,45 @@ import java.util.List;
 public class XMLBeanParser {
 
     public List<Bean> getBeansFrom(String fileName) throws IOException {
-        URL resource = Resources.getResource(fileName);
-        InputStream inputStream = resource.openStream();
-        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-        List<Bean> beans = Lists.newArrayList();
         try {
-            DocumentBuilder builder = dbf.newDocumentBuilder();
-            Document doc = builder.parse(inputStream);
-            Element root = doc.getDocumentElement();
-            NodeList childNodes = root.getChildNodes();
-            for (int i = 0; i < childNodes.getLength(); i++){
-                Node item = childNodes.item(i);
-                if (item.getNodeName().equals("bean")){
-                    NamedNodeMap attributes = item.getAttributes();
-                    String className = attributes.getNamedItem("class").getTextContent();
-                    String id = attributes.getNamedItem("id").getTextContent();
-                    beans.add(new Bean(className, id));
-                }
-            }
+            Document doc = getDocumentFromXML(fileName);
+            return findBeans(doc);
         } catch (ParserConfigurationException e) {
             e.printStackTrace();
         } catch (SAXException e) {
             e.printStackTrace();
         }
+        return null;
+    }
+
+    private List<Bean> findBeans(Document doc) {
+        List<Bean> beans = Lists.newArrayList();
+        Element bean = doc.getDocumentElement();
+        NodeList childNodes = bean.getChildNodes();
+        for (int i = 0; i < childNodes.getLength(); i++){
+            Node item = childNodes.item(i);
+            if (isABean(item)){
+                String className = getAttributesTextByName(item, "class");
+                String id = getAttributesTextByName(item, "id");
+                beans.add(new Bean(className, id));
+            }
+        }
         return beans;
+    }
+
+    private String getAttributesTextByName(Node item, String attributeName) {
+        return item.getAttributes().getNamedItem(attributeName).getTextContent();
+    }
+
+    private boolean isABean(Node item) {
+        return item.getNodeName().equals("bean");
+    }
+
+    private Document getDocumentFromXML(String fileName) throws IOException, ParserConfigurationException, SAXException {
+        URL resource = Resources.getResource(fileName);
+        InputStream inputStream = resource.openStream();
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = dbf.newDocumentBuilder();
+        return builder.parse(inputStream);
     }
 }
